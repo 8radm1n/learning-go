34c815dac099df30f2f15dfdf238649b902b74a5
Brad Searle <bradleysearle@gmail.com>
2020-09-05 10:36:20 +0000

    section 3 lessson 33

--- go-code/go-the-complete-developers-guide/cards/deck_test.go
+++ go-code/go-the-complete-developers-guide/cards/deck_test.go
@@ -4,8 +4,17 @@ import "testing"
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
-
+	expectedfirstCard := "Ace of Spades"
+	expectedlastCard := "King of Clubs"
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52 got: %v", len(d))
+	}
+
+	if d[0] != expectedfirstCard {
+		t.Errorf("Expected %v got: %v", expectedfirstCard, d[0])
+	}
+
+	if d[len(d)-1] != expectedlastCard {
+		t.Errorf("Expected %v got: %v", expectedlastCard, d[len(d)-1])
 	}
 }
