9276aa784c2cf70af0d02241055cdb6ad23ceba3
Brad Searle <bradleysearle@gmail.com>
2020-09-06 00:39:43 +0000

    section 3 lessson 39

--- go-code/go-the-complete-developers-guide/structs/main.go
+++ go-code/go-the-complete-developers-guide/structs/main.go
@@ -9,7 +9,13 @@ type person struct {
 }
 
 func main() {
-	// Shorthand syntax
+	// Create person struct with zero values assigned.
+	// Zero value for a string is ""
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+
+	// Shorthand syntax to create and assign properties
 	// alex := person{"Alex", "Anderson"}
 
 	// Preferred method to create struct instance
@@ -18,4 +24,7 @@ func main() {
 		lastName:  "Anderson",
 	}
 	fmt.Println(alex)
+
+	// Print properties and values with %+v
+	fmt.Printf("%+v", alex)
 }
