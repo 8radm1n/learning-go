21b9a73634683c7b8482bb71ba1ac7d7c3cb7fbc
Brad Searle <bradleysearle@gmail.com>
2020-09-11 05:30:16 +0000

    section 6 lesson 64

--- go-code/go-the-complete-developers-guide/http/main.go
+++ go-code/go-the-complete-developers-guide/http/main.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
-	resp, err := http.Get("http://google.com")
+	resp, err := http.Get("http://codingpackets.com")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	bs := make([]byte, 99999)
-	// The read function adds the content read
-	// to the byte slice passed into it.
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	io.Copy(os.Stdout, resp.Body)
 }
